506fc6d73952d7ae8756a94327d0c2b3ab6952bd
papabn <pndiaye999@gmail.com>
2023-09-17 23:19:46 +0000

    errors handler and SIGTERM

--- whatsapp_be/src/index.js
+++ whatsapp_be/src/index.js
@@ -8,6 +8,38 @@ import logger from './configs/logger.config.js';
 const PORT = process.env.PORT || 8000;
 console.log(process.env.NODE_ENV);
 
-app.listen(PORT,()=>{
+let server;
+
+server = app.listen(PORT,()=>{
     logger.info(`Server is running on port ${PORT}`);
+    // throw new Error("error is server");
+});
+
+
+// handle server error
+const exitHandler=()=>{
+    if(server){
+        logger.info("server closed.");
+        process.exit(1);
+        
+    }else{
+        process.exit(1)
+    }
+}
+
+    
+const unexpectedErrorHandler=(error)=>{
+    logger.error(error);
+    exitHandler();
+}
+
+process.on("uncaughtException",unexpectedErrorHandler);
+process.on("unhandledRejection",unexpectedErrorHandler);
+
+// SIGTERM
+process.on("SIGTERM",()=>{
+    if(server){
+        logger.info("Server closed.");
+        process.exit(1);
+    }
 });
