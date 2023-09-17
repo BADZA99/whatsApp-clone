1296a325d99901fee4a59d58eb98d9d3215cac32
papabn <pndiaye999@gmail.com>
2023-09-17 23:51:52 +0000

    handling http errors

--- whatsapp_be/src/app.js
+++ whatsapp_be/src/app.js
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import fileUpload from 'express-fileupload';
 import cors from 'cors';
+import createHttpError from "http-errors";
 
 // dotEnv config
 dotenv.config();
@@ -49,9 +50,24 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 
-
 app.post("/test", (req, res) => {
-  res.send("Hello World from server");
+  // res.send("Hello World from server");
+  throw createHttpError.BadRequest('this route has an error');
+});
+
+app.use(async(req,res,next)=>{
+    next(createHttpError.NotFound("this route does not exist"));
+})
+
+// error http handling
+app.use( async(err,req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
 });
 
 export default app;
