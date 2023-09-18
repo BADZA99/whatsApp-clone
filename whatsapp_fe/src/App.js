3cc55abe6a0bd87dc3da47cfdaf8915a633d6e2a
papabn <pndiaye999@gmail.com>
2023-09-18 16:02:04 +0000

    CRA and setup front end

---
+++ whatsapp_fe/src/App.js
@@ -0,0 +1,14 @@
+import './index.css';
+
+import { ChatIcon } from "./svg";
+
+function App() {
+  return (
+    <div className="">
+      <h1>hello from font end</h1>
+      <ChatIcon className="red"/>
+    </div>
+  );
+}
+
+export default App;
