1f3fed65f2148800c7614d0f9ce9edd2f8dc4eb0
papabn <pndiaye999@gmail.com>
2023-09-18 01:13:52 +0000

    Connect to Mngodb, handle connection errors

--- whatsapp_be/src/index.js
+++ whatsapp_be/src/index.js
@@ -1,4 +1,5 @@
 import app from "./app.js";
+import mongoose from "mongoose"
 // logger permet de personnaliser notre terminal histoire de savoir ce qui se passe c a d differiencer les erreurs des informations
 import logger from './configs/logger.config.js';
 
@@ -6,7 +7,37 @@ import logger from './configs/logger.config.js';
 
 // env variables
 const PORT = process.env.PORT || 8000;
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
+const {DATABASE_URL}=process.env;
+
+// exit on mongodb error
+mongoose.connection.on("error",(err)=>{
+    logger.error(`mongodb connection error : ${err}`);
+    process.exit(1);
+})
+
+
+
+// mongoo debug mode  (pour afficher TOUT ce qui se passse)
+if(process.env.NODE_ENV !== "production"){
+    mongoose.set("debug",true);
+}
+
+
+
+// connect to mongodb database
+mongoose.connect(DATABASE_URL,{
+    
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    logger.info("Connected to MongoDB");
+})
+
+
+
+
+
 
 let server;
 
