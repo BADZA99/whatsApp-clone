62e943296add85ac851b1757b8970b680c1a2471
papabn <pndiaye999@gmail.com>
2023-09-18 16:55:41 +0000

    setup react-router-dom

--- whatsapp_fe/src/App.js
+++ whatsapp_fe/src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import './index.css';
+import Home from './pages/home';
+import Login from './pages/login';
+import Register from './pages/register';
 // import { ChatIcon } from "./svg";
 
 function App() {
   return (
     <div className="dark">
-      <h1 className='custumh1'>Ilove tailwindcss</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
