873cef8a0cdfb6c420a87b824635d728cb1174ff
papabn <pndiaye999@gmail.com>
2023-10-05 02:37:06 +0000

    Chat slice

--- whatsapp_fe/src/app/store.js
+++ whatsapp_fe/src/app/store.js
@@ -3,6 +3,7 @@ import storage from "redux-persist/lib/storage";
 import userSlice from "../features/userSlice";
 import {persistReducer,persistStore} from "redux-persist";
 import createFilter from "redux-persist-transform-filter";
+import chatSlice from "../features/chatSlice";
 
 // saved only user saveUserOnlyFilter
 const saveUserOnlyFilter = createFilter("user",["user"]);
@@ -20,6 +21,7 @@ const persistConfig={
 
 const rootReducer=combineReducers({
     user:userSlice,
+    chat:chatSlice
 });
 
 const persistedReducer=persistReducer(persistConfig,rootReducer);
